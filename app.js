19d3d51c91b3428068b668b94c6c32124b7e872d
Logan Bestwick <loganbestwick@gmail.com>
2014-01-27 15:10:46 -0800

    end of learning

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
+
 var express = require('express');
+var hbs = require('hbs');
+
+var blogEngine = require('./blog');
+
 var app = express();
+
  
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+app.use(express.bodyParser());
+
 app.get('/', function(request, response){
-	response.sendfile('./views/index.html');
+	response.render('index', {title: "My Blog", entries : blogEngine.getBlogEntries()});
 });
 
 app.get('/about', function(request, response){
-	response.sendfile('./views/about.html');
+	response.render('about', {title : "About Me"});
 });
 
-app.get('/article', function(request, response){
-	response.sendfile('./views/article.html');
+app.get('/article/:id', function(request, response){
+	console.log(request.params)
+	var entry = blogEngine.getBlogEntry(request.params.id);
+	response.render('article', {title: entry.title, blog:entry});
 });
 
 app.listen(3000);
