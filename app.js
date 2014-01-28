6717e613668eac3d91ef62ffd33525aa93b2db9a
Logan Bestwick <loganbestwick@gmail.com>
2014-01-27 19:58:10 -0800

    async loaded, all methods in place

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var geoMethods = require('geo-methods');
 var hbs = require('hbs');
+var https = require('https');
+var request = require('request');
+var async = require("async");
 var app = express();
  
 app.set('view engine', 'html');
@@ -13,8 +16,36 @@ app.get('/', function(request, response){
 });
 
 app.get('/search', function(request, response){
-	var search_location = geoMethods.get_longitude_latitude(request.query.search_address);
-	var url = "https://data.sfgov.org/resource/rqzj-sfat.json"
+	var searchLocation = geoMethods.get_longitude_latitude(request.query.search_address);
+	
+	var options = {host: 'data.sfgov.org', path: '/resource/rqzj-sfat.json', method: 'GET'}
+	var truckData = '';
+	https.get(options, function(response){
+		response.on('data', function(chunk){
+			truckData += chunk;
+		});
+		response.on('end', function(){
+			JSON.parse(truckData);
+		})
+	})
+
+	var truck_results = findTrucks(truckData, searchLocation, parseFloat(request.query.search_address));
+
+
+
 });
+
+
+var findTrucks = function(trucksArray, searchLocation, range){
+	var trucksInRange = [];
+	truckArray.forEach(function(truck){
+		var distance = geoMethods.coorDist(searchLocation.latitude, searchLocation.longitude, 
+			parseFloat(truck.latitude), parseFloat(truck.longitude));
+		if (distance <= range) {
+			truckInRange.push(truck);
+		}
+	});
+	return trucksInRange;
+}
 
 app.listen(3000);
